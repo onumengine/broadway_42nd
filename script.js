c7714072f766223b4cdfc59dcdc3769d43206496
onumengine <onumdev@gmail.com>
2021-03-23 23:48:20 +0100

    make the about button toggle the about tab visibility

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+//jshint esversion: 6
+
 document.querySelectorAll(".tabContent").forEach((element) => element.classList.add("hidden"));
+
+document.querySelector("#aboutButton").addEventListener("click", () => {
+    document.querySelector("#aboutTab").classList.toggle("hidden");
+});
