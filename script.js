64f464d5c71899d95016ea8c0a425404485ab680
onumengine <onumdev@gmail.com>
2021-03-23 23:57:35 +0100

    wrap the picturegrid in a div

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //jshint esversion: 6
 
-document.querySelectorAll(".tabContent").forEach((element) => element.classList.add("hidden"));
+//document.querySelectorAll(".tabContent").forEach((element) => element.classList.add("hidden"));
 
 document.querySelector("#aboutButton").addEventListener("click", () => {
     document.querySelector("#aboutTab").classList.toggle("hidden");
