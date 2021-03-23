af65da26086a75462da19d5f14e8392ddfc4d80f
onumengine <onumdev@gmail.com>
2021-03-24 00:12:30 +0100

    make a page object to handle page behaviour

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 //jshint esversion: 6
 
-//document.querySelectorAll(".tabContent").forEach((element) => element.classList.add("hidden"));
+
+var page = {
+    allTabs: document.querySelectorAll(".tabContent"),
+    homeTab: document.querySelector("#homeTab"),
+    aboutTab: document.querySelector("#aboutTab"),
+    hideAllTabs() {
+        this.allTabs.forEach((element) => element.classList.add("hidden"));
+    },
+    showElement(elementId) {
+        document.querySelector(elementId).classList.add("visible");
+    },
+    toggleVisibility(elementId) {
+        document.querySelector(elementId).classList.toggle("hidden");
+    }
+};
 
 document.querySelector("#aboutButton").addEventListener("click", () => {
-    document.querySelector("#aboutTab").classList.toggle("hidden");
+    page.toggleVisibility("#aboutTab");
 });
+
+page.hideAllTabs();
+page.showElement("#homeTab");
