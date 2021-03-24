322dd4dda163043738c27590d89cf4103aa5b21f
onumengine <onumdev@gmail.com>
2021-03-24 16:11:44 +0100

    delete all console.logs

--- script.js
+++ script.js
@@ -14,14 +14,12 @@ const elementsIds = {
 const tabManager = {
     hideAllTabs: function() {
         var allTabsInPage = document.querySelectorAll(".tabContent");
-        //console.log(allTabsInPage);
         allTabsInPage.forEach((tab) => {
             tab.classList.remove("visible");
             tab.classList.add("hidden");
         });
     },
     showTabById: function(event) {
-        //console.log(`the ${event.target.id} was clicked`);
         switch(event.target.id) {
             case elementsIds.homeButton:
                 tabManager.showTab(elementsIds.homeTab);
@@ -41,15 +39,12 @@ const tabManager = {
     },
     showTab: function(tabId) {
         this.hideAllTabs();
-        //console.log(`making the ${tabId} visible...`);
         document.getElementById(tabId).classList.add("visible");
-        //console.log(`attempt to display ${tabId} complete`);
     }
 };
 
 function main() {
     var navbarButtons = document.querySelectorAll("#appbarActions li");
-    //console.log(navbarButtons);
 
     navbarButtons.forEach((button) => {button.addEventListener("click", tabManager.showTabById);});
 
